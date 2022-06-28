f3d312e8eae6b29fa3faaff506840e0bf2638214
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-28 14:22:56 -0500

    bonus

--- src/ContactFileIO.java
+++ src/ContactFileIO.java
@@ -16,75 +16,86 @@ public class ContactFileIO {
         Path filepath = Paths.get("data", "contacts.txt");
         List<String> Lines;
         List<Contacts> contactsList = new ArrayList<>();
-        System.out.println("1. View Contacts \n2. Add new contact \n3. Search contact by name \n4. Delete an existing contact. \n5. Exit \nEnter an option (1, 2, 3, 4, or 5)");
 
-        Integer input = scn.nextInt();
-        if (input == 1) {
-            try {
-                Lines = Files.readAllLines(filepath);
-                for (String Line : Lines) {
-                    contactsList.add(new Contacts());
-                    if (Line.equalsIgnoreCase("")) {
-                        continue;
+        while (true) {
+            System.out.println("1. View Contacts \n2. Add new contact \n3. Search contact by name \n4. Delete an existing contact. \n5. Exit \nEnter an option (1, 2, 3, 4, or 5)");
+
+            Integer input = scn.nextInt();
+            if (input == 1) {
+                try {
+                    Lines = Files.readAllLines(filepath);
+                    for (String Line : Lines) {
+                        contactsList.add(new Contacts());
+                        if (Line.equalsIgnoreCase("")) {
+                            continue;
+                        }
+                        System.out.println(Line);
                     }
-                    System.out.println(Line);
+                } catch (IOException i) {
+                    throw new RuntimeException(i);
                 }
-            } catch (IOException i) {
-                throw new RuntimeException(i);
             }
-        }
-        if (input == 2) {
-            try {
-                Lines = Files.readAllLines(filepath);
-                System.out.println("Enter contact name");
-                scn.nextLine();
-                String contactName = scn.nextLine();
-                System.out.println("Enter contact number");
-                String contactNumber = scn.nextLine();
-                String contactFormat = String.format("%s: %s", contactName, contactNumber);
-                Files.write(filepath, Arrays.asList(contactFormat), StandardOpenOption.APPEND);
-                Lines.add(contactFormat);
-                contactsList.add(new Contacts(contactName, contactNumber));
+            if (input == 2) {
+                try {
+                    Lines = Files.readAllLines(filepath);
+                    System.out.println("Enter contact name");
+                    scn.nextLine();
+                    String contactName = scn.nextLine();
+                    System.out.println("Enter contact number");
+                    String contactNumber = scn.nextLine();
+                    String contactFormat = String.format("%s: %s", contactName, contactNumber);
+                    Files.write(filepath, Arrays.asList(contactFormat), StandardOpenOption.APPEND);
+                    Lines.add(contactFormat);
+                    contactsList.add(new Contacts(contactName, contactNumber));
 //                for (Contacts contact: contactsList) {
 //                    System.out.println(contact);
 //                }
-            } catch (IOException o) {
-                o.printStackTrace();
-            }
-        }
-        if (input == 3) {
-            try {
-                Lines = Files.readAllLines(filepath);
-                System.out.println("Enter the name you'd like to search");
-                scn.nextLine();
-                String nameSearch = scn.nextLine();
-                for (String Line : Lines) {
-                    contactsList.add(new Contacts());
-                    if (Line.contains(nameSearch) == true) {
-                        System.out.println(Line);
-                    }
+                } catch (IOException o) {
+                    o.printStackTrace();
                 }
-            } catch (IOException o) {
-                o.printStackTrace();
             }
-        }
-        if (input == 4) {
-            try {
-                Lines = Files.readAllLines(Paths.get("data", "contacts.txt"));
-                List<String> newList = new ArrayList<>();
-                System.out.println("Enter a contact you'd like to delete");
-                scn.nextLine();
-                String deleteName = scn.nextLine();
-                for (String Line : Lines) {
-                    if (Line.contains(deleteName) == true) {
-                        newList.add("");
-                        continue;
+            if (input == 3) {
+                try {
+                    Lines = Files.readAllLines(filepath);
+                    System.out.println("Enter the name you'd like to search");
+                    scn.nextLine();
+                    String nameSearch = scn.nextLine();
+                    for (String Line : Lines) {
+                        contactsList.add(new Contacts());
+                        if (Line.contains(nameSearch) == true) {
+                            System.out.println(Line);
+                        }
                     }
-                    newList.add(Line);
+                } catch (IOException o) {
+                    o.printStackTrace();
                 }
-                Files.write(Paths.get("data", "contacts.txt"), newList);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            }
+            if (input == 4) {
+                try {
+                    Lines = Files.readAllLines(Paths.get("data", "contacts.txt"));
+                    List<String> newList = new ArrayList<>();
+                    System.out.println("Enter a contact you'd like to delete");
+                    scn.nextLine();
+                    String deleteName = scn.nextLine();
+                    for (String Line : Lines) {
+                        if (Line.contains(deleteName) == true) {
+                            newList.add("");
+                            continue;
+                        }
+                        newList.add(Line);
+                    }
+                    Files.write(Paths.get("data", "contacts.txt"), newList);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (input == 5) {
+                break;
+            }
+            System.out.println("Would you like to select another option? [Y/N]");
+            String decision = scn.next();
+            if (decision.equalsIgnoreCase("n")){
+                break;
             }
         }
     }
